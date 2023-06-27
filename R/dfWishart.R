5fce7db35097fe59ca29dd69c2a6849eab23014e
Matthew L. Fidler <matthew.fidler@gmail.com>
2023-06-27 11:39:43 +0200

    Export and expand dfWishart

--- R/dfWishart.R
+++ R/dfWishart.R
@@ -1,18 +1,52 @@
-dfWishartCalcRse <- function(nu, omega, totN, rse) {
+dfWishartCalcRse <- function(nu, omega, totN, rse, diag=TRUE) {
   .cv <- cvPost(nu, omega, totN)
   #.cv <- ivdoctr:::rinvwish(totN, nu, omega)
   #print(.cv)
   #max(omegaListRse(.cv)$rse)-rse
-  mean(diag(omegaListRse(.cv)$sd))-rse
+  if (diag) {
+    mean(diag(omegaListRse(.cv)$rse))-rse
+  } else {
+    mean(as.vector(omegaListRse(.cv)$rse))-rse
+  }
 }
 
-dfWishart <- function(omega, n, rse, totN=1000) {
+#' This uses simulations to match the rse 
+#'
+#' @param omega represents the matrix for simulation
+#' @param n This represents the number of subjects/samples this comes
+#'   from (used to calculate rse).  When present it assumes the rse=
+#'   sqrt(2)/sqrt(n)
+#' @param rse This is the rse that we try to match, if not specified,
+#'   it is derived from `n`
+#' @param upper The upper boundary for root finding in terms of
+#'   degrees of freedom.  If not specified, it is n*200
+#' @param totN This represents the total number of simulated inverse wishart deviates
+#' @param diag When `TRUE`, represents the rse to match is the diagonals, otherwise it is the total matrix.
+#' @return output from `uniroot()` to find the right estimate
+#' @export
+#' @author Matthew L. Fidler
+#' @examples
+#'
+#' dfWishart(lotri::lotri(a+b~c(1, 0.5, 1)), 100)
+#' 
+dfWishart <- function(omega, n, rse, upper, totN=1000, diag=TRUE, seed=1234) {
   checkmate::assertMatrix(omega, "numeric", min.rows=1, min.cols=1)
-  checkmate::assertIntegerish(n, len=1, lower=1)
-  checkmate::assertIntegerish(totN, len=1, lower=1)
-  if (missing(rse)) {
-    rse <- sqrt(2)/sqrt(n)
+  if (!missing(rse) && !missing(n)) {
+    stop("can only specify `n` or `rse` not both", call.=FALSE)
   }
+  if (missing(rse) && !missing(n)) {
+    checkmate::assertIntegerish(n, len=1, lower=1)
+    rse <- sqrt(2)/sqrt(n)
+    if (missing(upper)) {
+      upper <- 200*n
+    }
+  } else if (missing(rse)) {
+    stop("need to match rse with some metric", call.=FALSE)
+  } else if (missing(upper)) {
+    upper <- 200*(sqrt(2)/rse)^2
+  }
+  checkmate::assertNumeric(upper, len=1, lower=1)
+  checkmate::assertIntegerish(totN, len=1, lower=1)
   .d <-  dim(omega)
   if (.d[1] != .d[2]) {
     stop("omega must be a square matrix",
@@ -20,6 +54,8 @@ dfWishart <- function(omega, n, rse, totN=1000) {
   }
   # nu-p-3 > 0 so min for nu is
   .lower <- .d[1] + 3.1
-  .upper <-  n*200
-  uniroot(dfWishartCalcRse, lower=.lower, upper=.upper, omega=omega, totN=totN, rse=rse)
+  .upper <-  upper
+  rxWithSeed(seed, {
+    uniroot(dfWishartCalcRse, lower=.lower, upper=.upper, omega=omega, totN=totN, rse=rse, diag=diag)
+  })
 }
